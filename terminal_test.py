596eec4ed46f01ab06653d95c6e6e067b8354561
“aider” <aider@bilibili.com>
2025-05-05 19:46:34 +0800

    fix: update test commands with new parameter format

--- terminal_test.py
+++ terminal_test.py
@@ -4,10 +4,10 @@ import time
 # 测试终端命令控制
 def test_terminal_commands():
     commands = [
-        "forward 4 5",    # 前进速度4，距离5
-        "right 90",       # 右转90度
-        "forward 2 3",    # 前进速度2，距离3
-        "left 45",        # 左转45度
+        "forward 1 4 5",    # 前进速度4，距离5
+        "right 1 90",       # 右转90度
+        "forward 1 2 3",    # 前进速度2，距离3
+        "left 1 45",        # 左转45度
         "stop"            # 停止
     ]
     
